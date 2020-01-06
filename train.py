02443f908f494367652d0475e1b87db954cb459f
Glitch <>
2020-01-06 21:04:30 +0900

    学習処理修正.

--- train.py
+++ train.py
@@ -11,10 +11,10 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, Ear
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
-
+import sys
 
 def _main():
-    annotation_path = 'train.txt'
+    annotation_path = 'model_data/2007_train.txt'
     log_dir = 'logs/000/'
     classes_path = 'model_data/voc_classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
@@ -22,7 +22,9 @@ def _main():
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
 
-    input_shape = (416,416) # multiple of 32, hw
+    input_shape = (320,320) # multiple of 32, hw
+    if len(sys.argv) > 1:
+        input_shape = (int(sys.argv[1]),int(sys.argv[1]))
 
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
@@ -55,6 +57,8 @@ def _main():
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
         batch_size = 32
+        if len(sys.argv) > 2:
+            batch_size = int(sys.argv[2])
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
